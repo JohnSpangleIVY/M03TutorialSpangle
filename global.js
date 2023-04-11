1f55896957239e1ed2ba170c5d179b7efea3c121
JohnSpangleIVY <128533067+JohnSpangleIVY@users.noreply.github.com>
2023-04-11 01:39:09 +0000

    All files and code from M03's Tutorial (Node.js). -JS

---
+++ global.js
@@ -0,0 +1,17 @@
+// Global object
+
+// console.log(global);
+
+// setTimeOut(() => {
+//   console.log('in the timeout');
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log('in the interval');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(document.querySelector);
